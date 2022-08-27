21bc87a48a05c8599d5425a31871c12287d0a380
Yaroslav <96979627+Pytlabg@users.noreply.github.com>
2022-08-27 18:03:14 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from pygismeteo import Gismeteo
+import telebot
+
+bot = telebot.TeleBot("5594186231:AAFxJWorbJxeHCZFwGXZVAdOVa_Yt9Wd26c")
+gismeteo = Gismeteo()
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет,Назови свой город")
+@bot.message_handler(content_types=['text'])
+def send_welcome(message):
+    search_results = gismeteo.search.by_query( message.text )
+    city_id = search_results[0].id
+    current = gismeteo.current.by_id(city_id)
+    answer = "В " + "Твоем городе/селе" + " " + str(current.temperature.air.c) + "\n"
+    bot.reply_to(message, answer)
+bot.infinity_polling()
