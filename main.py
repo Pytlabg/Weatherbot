8d4814e00236e3a9581128ee6ef246fe9182d233
AyonovDenizs <77270222+AyonovDenizs@users.noreply.github.com>
2022-08-29 21:30:26 +0600

    Fix akkuratnist pisma

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot("5594186231:AAFxJWorbJxeHCZFwGXZVAdOVa_Yt9Wd26c")
 gismeteo = Gismeteo()
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет,Назови свой город")
+    bot.reply_to(message, "Привет, назови свой город:")
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
     search_results = gismeteo.search.by_query( message.text )
